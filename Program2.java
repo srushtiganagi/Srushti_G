29d4b9a89122b23cb45334c282ef1c01eaa95c94
srushtiganagi <109858835+srushtiganagi@users.noreply.github.com>
2025-06-26 11:12:11 +0530

    Update Program2.java

--- Program2.java
+++ Program2.java
@@ -4,7 +4,7 @@ public class OddSeriesGenerator {
     public static void main(String[] args) {
         // input from user
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number (a): ");
+        System.out.print("Enter a number: ");
         int a = sc.nextInt();
 
         // Generate and print odd numbers
